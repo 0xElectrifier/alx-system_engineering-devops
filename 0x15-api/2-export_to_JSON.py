8a8044a6ce0f6fd9ecefe824fb4360f07b7ab56f
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-19 17:05:33 +0100

    Formatting output to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-"""
-Using https://jsonplaceholder.typicode.com
-gathers data from API and exports it to JSON file
-Implemented using recursion
+"""Extension of file '0-gather_data_from_an_API.py'.
+Exports data in JSON format
 """
 import json
-import re
-import requests
 import sys
-
-
-API = "https://jsonplaceholder.typicode.com"
-"""REST API url"""
+from urllib import request
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API, id)).json()
-            todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('username')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            with open("{}.json".format(id), 'w') as json_file:
-                user_data = list(map(
-                    lambda x: {
-                        "task": x.get("title"),
-                        "completed": x.get("completed"),
-                        "username": user_name
-                    },
-                    todos
-                ))
-                user_data = {
-                    "{}".format(id): user_data
-                }
-                json.dump(user_data, json_file)
+    url = 'https://jsonplaceholder.typicode.com/'
+    # Make sure that only an integer was passed
+    employee_id = int(sys.argv[1])
+    employee_id = str(employee_id)
+    with request.urlopen(url + 'users?id=' + employee_id) as res:
+        response = json.loads(res.read().decode())
+        username = response[0].get('username')
+
+    with request.urlopen(url + 'todos?userId=' + employee_id) as res:
+        response = json.loads(res.read().decode())
+
+    json_data = {}
+    json_data[employee_id] = [{"task": task.get('title'),
+        "completed": task.get('completed'),
+        "username": username} for task in response]
+    with open(employee_id + '.json', 'w') as file:
+        json.dump(json_data, file)
